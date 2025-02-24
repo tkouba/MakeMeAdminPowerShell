0df4232ea62f5900cc046ceacd9a5eb785fc8e60
Tomas Kouba <tkouba@users.noreply.github.com>
2025-02-24 10:34:27 +0100

    Script analyzer resolve

--- Grant-AdminGights.ps1
+++ Grant-AdminGights.ps1
@@ -13,13 +13,13 @@
 
 .TAGS admin-rights makemeadmin
 
-.LICENSEURI
+.LICENSEURI https://raw.githubusercontent.com/tkouba/MakeMeAdminPowerShell/refs/heads/master/LICENSE.txt
 
-.PROJECTURI
+.PROJECTURI https://github.com/tkouba/MakeMeAdminPowerShell
 
-.ICONURI
+.ICONURI https://raw.githubusercontent.com/tkouba/MakeMeAdminPowerShell/refs/heads/master/SecurityLock.ico
 
-.EXTERNALMODULEDEPENDENCIES 
+.EXTERNALMODULEDEPENDENCIES
 
 .REQUIREDSCRIPTS
 
@@ -32,20 +32,20 @@
 
 #>
 
-<# 
-.SYNOPSIS  
+<#
+.SYNOPSIS
  Grant temporary administrators right using Make Me Admin service.
 
-.DESCRIPTION 
+.DESCRIPTION
  Uses Make Me Admin service to make standard user account to administrator-level, on a temporary basis.
- See Make Me Admin wiki https://github.com/pseymour/MakeMeAdmin/wiki 
- 
+ See Make Me Admin wiki https://github.com/pseymour/MakeMeAdmin/wiki.
+
 .INPUTS
 NONE
- 
+
 .OUTPUTS
 NONE
- 
+
 .EXAMPLE
 C:\PS> .\Grant-AdminRights.ps1
 
@@ -56,21 +56,21 @@ Description
 .NOTES
 Version : 1.0, 2025-02-23
 Requires : PowerShell Desktop
- 
-.LINK 
-https://github.com/pseymour/MakeMeAdmin/wiki
 
-#> 
+.LINK
+https://github.com/pseymour/MakeMeAdmin/wiki
+https://github.com/tkouba/MakeMeAdminPowerShell
+
+#>
 [CmdletBinding()]
 Param()
-PROCESS {  
+PROCESS {
     #disable debug confirmation messages
-    if($PSBoundParameters['Debug']){$DebugPreference = "Continue"}
-    try { 
+    if ($PSBoundParameters['Debug']) { $DebugPreference = "Continue" }
+    try {
         $source = @"
         using System;
         using System.ServiceModel;
-        
         namespace SinclairCC.MakeMeAdmin
         {
             public enum RemovalReason : int
@@ -90,38 +90,38 @@ PROCESS {
             }
         }
 "@
-        
+
         # Import the required .NET assemblies
         Add-Type -TypeDefinition $source -ReferencedAssemblies System.ServiceModel
-        Add-Type -AssemblyName System.ServiceModel        
-        
+        Add-Type -AssemblyName System.ServiceModel
+
         # Load the required .NET assemblies
         [System.Reflection.Assembly]::LoadWithPartialName("System.ServiceModel") | Out-Null
-        
+
         # Create a NetNamedPipeBinding object
         $binding = New-Object System.ServiceModel.NetNamedPipeBinding
-        
+
         # Define the endpoint address
         $endpointAddress = "net.pipe://localhost/MakeMeAdmin/Service"
-        
+
         Write-Debug "Using endpoint address '$endpointAddress'"
 
         # Create a channel factory to communicate with the service
         $channelFactory = New-Object 'System.ServiceModel.ChannelFactory[SinclairCC.MakeMeAdmin.IAdminGroup]' -ArgumentList $binding, $endpointAddress
-        
+
         # Create a channel using the channel factory
         $channel = $channelFactory.CreateChannel()
-        
+
         # Call the AddUserToAdministratorsGroup method on the channel
         $channel.AddUserToAdministratorsGroup()
-        
+
         # Close the channel factory
-        $channelFactory.Close()     
-        
-        Write-Host 'You are now a member of the Administrators group.'
+        $channelFactory.Close()
+
+        Write-Output 'You are now a member of the Administrators group.'
     }
     catch {
         #show error message (non terminating error so that the rest of the pipe input get processed)
         Write-Error $_
-    }     
+    }
 }
--- Revoke-AdminGights.ps1
+++ Revoke-AdminGights.ps1
@@ -13,13 +13,13 @@
 
 .TAGS admin-rights makemeadmin
 
-.LICENSEURI
+.LICENSEURI https://raw.githubusercontent.com/tkouba/MakeMeAdminPowerShell/refs/heads/master/LICENSE.txt
 
-.PROJECTURI
+.PROJECTURI https://github.com/tkouba/MakeMeAdminPowerShell
 
-.ICONURI
+.ICONURI https://raw.githubusercontent.com/tkouba/MakeMeAdminPowerShell/refs/heads/master/SecurityLock.ico
 
-.EXTERNALMODULEDEPENDENCIES 
+.EXTERNALMODULEDEPENDENCIES
 
 .REQUIREDSCRIPTS
 
@@ -32,20 +32,20 @@
 
 #>
 
-<# 
-.SYNOPSIS  
+<#
+.SYNOPSIS
  Revoke temporary administrators right using Make Me Admin service.
 
-.DESCRIPTION 
+.DESCRIPTION
  Uses Make Me Admin service to remove user account from Administrators.
- See Make Me Admin wiki https://github.com/pseymour/MakeMeAdmin/wiki 
- 
+ See Make Me Admin wiki https://github.com/pseymour/MakeMeAdmin/wiki.
+
 .INPUTS
 NONE
- 
+
 .OUTPUTS
 NONE
- 
+
 .EXAMPLE
 C:\PS> .\Revoke-AdminRights.ps1
 
@@ -56,21 +56,21 @@ Description
 .NOTES
 Version : 1.0, 2025-02-25
 Requires : PowerShell Desktop
- 
-.LINK 
-https://github.com/pseymour/MakeMeAdmin/wiki
 
-#> 
+.LINK
+https://github.com/pseymour/MakeMeAdmin/wiki
+https://github.com/tkouba/MakeMeAdminPowerShell
+
+#>
 [CmdletBinding()]
 Param()
-PROCESS {  
+PROCESS {
     #disable debug confirmation messages
-    if($PSBoundParameters['Debug']){$DebugPreference = "Continue"}
-    try { 
+    if ($PSBoundParameters['Debug']) { $DebugPreference = "Continue" }
+    try {
         $source = @"
         using System;
         using System.ServiceModel;
-        
         namespace SinclairCC.MakeMeAdmin
         {
             public enum RemovalReason : int
@@ -90,39 +90,38 @@ PROCESS {
             }
         }
 "@
-        
+
         # Import the required .NET assemblies
         Add-Type -TypeDefinition $source -ReferencedAssemblies System.ServiceModel
-        Add-Type -AssemblyName System.ServiceModel        
-        
+        Add-Type -AssemblyName System.ServiceModel
+
         # Load the required .NET assemblies
         [System.Reflection.Assembly]::LoadWithPartialName("System.ServiceModel") | Out-Null
-        
+
         # Create a NetNamedPipeBinding object
         $binding = New-Object System.ServiceModel.NetNamedPipeBinding
-        
+
         # Define the endpoint address
         $endpointAddress = "net.pipe://localhost/MakeMeAdmin/Service"
-        
+
         Write-Debug "Using endpoint address '$endpointAddress'"
 
         # Create a channel factory to communicate with the service
         $channelFactory = New-Object 'System.ServiceModel.ChannelFactory[SinclairCC.MakeMeAdmin.IAdminGroup]' -ArgumentList $binding, $endpointAddress
-        
+
         # Create a channel using the channel factory
         $channel = $channelFactory.CreateChannel()
-        
+
         # Call the RemoveUserFromAdministratorsGroup method on the channel
         $channel.RemoveUserFromAdministratorsGroup([SinclairCC.MakeMeAdmin.RemovalReason]::UserRequest)
-        
+
         # Close the channel factory
-        $channelFactory.Close()     
-        
-        Write-Host 'You are no longer a member of the Administrators group.'
+        $channelFactory.Close()
+
+        Write-Output 'You are no longer a member of the Administrators group.'
     }
     catch {
         #show error message (non terminating error so that the rest of the pipe input get processed)
         Write-Error $_
-    }     
+    }
 }
-
