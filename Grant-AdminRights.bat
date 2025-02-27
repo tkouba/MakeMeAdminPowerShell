fd755cee859362ab9d196f00c7bb3d58ff999d01
Tomas Kouba <tkouba@users.noreply.github.com>
2025-02-27 10:17:36 +0100

    Batch (cmd) commands for running powershell

---
+++ Grant-AdminRights.bat
@@ -0,0 +1,2 @@
+@rem Grant temporary administrators right using Make Me Admin service and powershell script
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -File "%~dpn0.ps1"
